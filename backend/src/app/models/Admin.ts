764168ad2ba10bcf5aeafc58bd18ebaf8e298a30
Célio <celio1878@hotmail.com>
2020-05-03 11:48:24 -0300

    Update Admin.ts
    model admin

---
+++ backend/src/app/models/Admin.ts
@@ -0,0 +1,21 @@
+import bcrypt from 'bcrypt';
+import Sequelize, { Model } from 'sequelize';
+
+export default class Admin extends Model {
+	password_hash!: string;
+	static init(connect: any) {
+		super.init(
+			{
+				name: Sequelize.STRING,
+				email: Sequelize.STRING,
+				password_hash: Sequelize.STRING,
+			},
+			{ connect }
+		);
+		return this;
+	}
+
+	checkPassword(password: string) {
+		return bcrypt.compare(password, this.password_hash);
+	}
+}
